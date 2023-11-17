be6ee3f16a6437e84a1a6e1e80c43af2cc1134d1
SIDDIQST1 <siddiqst194@gmail.com>
2023-11-17 08:52:55 -0800

    Added a c files  for the print f priject

--- utils.c
+++ utils.c
@@ -1,86 +1,74 @@
 #include "main.h"
-
 /**
- * is_printable - Deterines chars printability
- * @c: Char to be evaluated.
- *
- * Return: 1 if c is printable, 0 otherwise
- */
+* is_printable - Evaluates if a char is printable
+* @c: Char to be evaluated.
+*
+* Return: 1 if c is printable, 0 otherwise
+*/
 int is_printable(char c)
 {
-	if (c >= 32 && c < 127)
-		return (1);
-
-	return (0);
+if (c >= 32 && c < 127)
+return (1);
+return (0);
 }
-
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars
- * @i: Index at which to start appending
- * @ascii_code: ASSCI CODE
- * Return: Always 3 (Success)
- */
+* append_hexa_code - Append ascci in hexadecimal code to buffer
+* @buffer: Array of chars
+* @i: Index at which to start appending
+* @ascii_code: ASSCI CODE
+* Return: Always 3 (Success)
+*/
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
-	if (ascii_code < 0)
-		ascii_code *= -1;
-
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
-
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
-
-	return (3);
+char map_to[] = "0123456789ABCDEF";
+/* The hexa format code is always 2 digits long */
+if (ascii_code < 0)
+ascii_code *= -1;
+buffer[i++] = '\\';
+buffer[i++] = 'x';
+buffer[i++] = map_to[ascii_code / 16];
+buffer[i] = map_to[ascii_code % 16];
+return (3);
 }
-
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
+* is_digit - Verifies if a char is a digit
+* @c: Char to be evaluated
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
 int is_digit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-
-	return (0);
+if (c >= '0' && c <= '9')
+return (1);
+return (0);
 }
-
 /**
- * convert_size_number - Casts a no. to the specified size
- * @num: No. to be casted.
- * @size: No. indicating the type to be casted.
- *
- * Return: Casted value of no.
- */
+* convert_size_number - Casts a no.  the specified size
+* @num: No. to be casted.
+* @size: No. indicating the type to be casted.
+*
+* Return: Casted value of num
+*/
 long int convert_size_number(long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((short)num);
-
-	return ((int)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((short)num);
+return ((int)num);
 }
-
 /**
- * convert_size_unsgnd - Casts a no. to the specified size
- * @num: No. to be casted
- * @size: No. indicating the type to be casted
- *
- * Return: Casted value of num
- */
+* convert_size_unsgnd - Casts a no. to the specified size
+* @num: No. to be casted
+* @size: No. indicating the type to be casted
+*
+* Return: Casted value of num
+*/
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
-
-	return ((unsigned int)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((unsigned short)num);
+return ((unsigned int)num);
 }
